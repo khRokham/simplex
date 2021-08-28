eeea5296063821931dabadcc892c55e0532cb3a2
khrokham <rokhamkhawaja.rk22@gmail.com>
2021-08-28 18:44:11 +0500

    proper edit header

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>
-            navbar
+        <div className="navBar-wrapper">
+            <div className="logo">simplex</div>
+            <ul className="nav">
+                <li className="list-item"><a href= "#">Home</a></li>
+                <li className="list-item"><a href= "#">Services</a></li>
+                <li className="list-item"><a href= "#">Pricing</a></li>
+                <li className="list-item"><a href= "#">Contact-Us</a></li>
+                <li className="list-item"><a href= "#">About-Us</a></li>
+            </ul>
         </div>
     )
 }
