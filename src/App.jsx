99b5a74dc0302f240922c233ca91b07133bdf8e6
khrokham <rokhamkhawaja.rk22@gmail.com>
2021-08-28 18:12:08 +0500

    basic set up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/header/Navbar'
+import Footer from './components/footer/Footer'
+const App = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default App
